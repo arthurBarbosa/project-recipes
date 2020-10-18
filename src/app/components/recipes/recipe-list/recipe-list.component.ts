3c8c860ad5883f3451a084174e9a4bff32a0f98d
Arthur Barbosa <arthurbruno03@gmail.com>
2020-10-17 21:40:38 -0300

    population data in recipe list

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/stuffed-pasta-bake-bolognese-2a0e5e3.jpg?quality=90&resize=500%2C454')
+  ];
   constructor() { }
 
   ngOnInit(): void {
